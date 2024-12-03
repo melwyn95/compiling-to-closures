b1f2ca188177d7f7a29db98cd6ace3e34e6161f3
Melwyn Saldanha <melwyn95@gmail.com>
2024-12-03 09:44:51 -0500

    csci641: add benchmarks for tak and sort

--- haskell/bench/Bench.hs
+++ haskell/bench/Bench.hs
@@ -5,12 +5,26 @@ import Programs
 
 main :: IO ()
 main =
-  let compiledProgram = compile fibExpr
-   in defaultMain
-        [ bgroup
+  defaultMain
+    [ let compiledProgram = compile fibExpr
+       in bgroup
             "fib"
             [ bench "impl-lang" $ whnf fib 20,
               bench "interpreter" $ whnf interpret fibExpr,
               bench "compiler" $ whnf evaluate $! compiledProgram
+            ],
+      let compiledProgram = compile takExpr
+       in bgroup
+            "tak"
+            [ bench "impl-lang" $ whnf (\(a, b, c) -> tak a b c) (18, 12, 6),
+              bench "interpreter" $ whnf interpret takExpr,
+              bench "compiler" $ whnf evaluate $! compiledProgram
+            ],
+      let compiledProgram = compile sortExpr
+       in bgroup
+            "sort"
+            [ bench "impl-lang" $ whnf sort pie,
+              bench "interpreter" $ whnf interpret sortExpr,
+              bench "compiler" $ whnf evaluate $! compiledProgram
             ]
-        ]
+    ]
