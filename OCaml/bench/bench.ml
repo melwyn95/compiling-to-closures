078c151eee9607ddaeb6f72afda926b1046e40b8
Melwyn Saldanha <melwyn95@gmail.com>
2024-12-05 11:40:14 -0500

    csci641: benchmarks for OCaml implementation

--- OCaml/bench/bench.ml
+++ OCaml/bench/bench.ml
@@ -1,36 +1,40 @@
-open Core
 open Core_bench
+open Lib.Programs
+open Lib.Scheme
 
-let get_float () = if Random.bool () then 10.0 else 10.0
-let get_int () = Random.int 200000
-let get_int64 () = if Random.bool () then Int64.of_int 10 else 10L
-let scale t mul = Stdlib.int_of_float (Stdlib.float_of_int t *. mul)
-let t1 = Bench.Test.create ~name:"Id" (fun () -> ())
+let fib =
+  Bench.Test.create_group ~name:"fib"
+    [
+      Bench.Test.create ~name:"impl-lang" (fun () -> C (fib 20));
+      Bench.Test.create ~name:"interpreter" (fun () ->
+          Lib.Interpreter.interpret fib_expr);
+      (let fib_program = Lib.Compiler.compile fib_expr in
+       Bench.Test.create ~name:"compiler" (fun () ->
+           Lib.Compiler.evaluate fib_program));
+    ]
 
-let t2 =
-  let n = get_int () in
-  let fl = get_float () in
-  Bench.Test.create ~name:"integer scaling" (fun () -> ignore (scale n fl))
+let tak =
+  Bench.Test.create_group ~name:"tak"
+    [
+      Bench.Test.create ~name:"impl-lang" (fun () -> C (tak 18 12 6));
+      Bench.Test.create ~name:"interpreter" (fun () ->
+          Lib.Interpreter.interpret tak_expr);
+      (let tak_program = Lib.Compiler.compile tak_expr in
+       Bench.Test.create ~name:"compiler" (fun () ->
+           Lib.Compiler.evaluate tak_program));
+    ]
 
-let t3 =
-  Bench.Test.create ~name:"Int64.bits_of_float"
-    (let fl = get_float () in
-     fun () -> ignore (Int64.bits_of_float fl))
+let sort =
+  Bench.Test.create_group ~name:"sort"
+    [
+      Bench.Test.create ~name:"impl-lang" (fun () ->
+          Lst (List.map (fun n -> C n) @@ sort pie));
+      Bench.Test.create ~name:"interpreter" (fun () ->
+          Lib.Interpreter.interpret sort_expr);
+      (let sort_program = Lib.Compiler.compile sort_expr in
+       Bench.Test.create ~name:"compiler" (fun () ->
+           Lib.Compiler.evaluate sort_program));
+    ]
 
-let t4 =
-  Bench.Test.create ~name:"Int64.float_of_bits"
-    (let fl = get_int64 () in
-     fun () -> ignore (Int64.float_of_bits fl))
-
-let t5 =
-  let f1 = Random.float 1.0 in
-  let f2 = Random.float 1.0 in
-  Bench.Test.create ~name:"Float.*" (fun () -> ignore (f1 *. f2))
-
-let t6 =
-  let f1 = Random.int 5000 in
-  let f2 = Random.int 5000 in
-  Bench.Test.create ~name:"Int.*" (fun () -> ignore (f1 * f2))
-
-let tests = [ t1; t2; t3; t4; t5; t6 ]
+let tests = [ fib; tak; sort ]
 let () = Command_unix.run (Bench.make_command tests)
